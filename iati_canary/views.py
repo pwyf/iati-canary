03445ec7111aaf2d1be1be5762256341a7ff1b06
Andy Lulham <a.lulham@gmail.com>
2019-03-13 15:00:55 +0000

    Add some basic templates

--- iati_canary/views.py
+++ iati_canary/views.py
@@ -1,10 +1,22 @@
-from flask import Blueprint
+from flask import abort, Blueprint, render_template
+from peewee import DoesNotExist
+
+from . import models
 
 
-blueprint = Blueprint('.', __name__,
+blueprint = Blueprint('iati_canary', __name__,
                       static_folder='../static')
 
 
 @blueprint.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return render_template('home.html', publishers=models.Publisher)
+
+
+@blueprint.route('/publisher/<publisher_id>')
+def publisher(publisher_id):
+    try:
+        publisher = models.Publisher.get_by_id(publisher_id)
+    except DoesNotExist:
+        return abort(404)
+    return render_template('publisher.html', publisher=publisher)
