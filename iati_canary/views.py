6c2507772b41b41e26da804dec9e74dd74237a0d
Andy Lulham <a.lulham@gmail.com>
2019-03-14 13:21:14 +0000

    Add an initial /errors view

--- iati_canary/views.py
+++ iati_canary/views.py
@@ -1,5 +1,5 @@
 from flask import abort, Blueprint, render_template
-from peewee import DoesNotExist
+from peewee import DoesNotExist, fn
 
 from . import models
 
@@ -22,3 +22,15 @@ def publisher(publisher_id):
     except DoesNotExist:
         return abort(404)
     return render_template('publisher.html', publisher=publisher)
+
+
+@blueprint.route('/errors')
+def errors():
+    publishers = (models.Publisher
+                  .select(models.Publisher,
+                          fn.Count(models.DatasetError.id).alias('count'))
+                  .join(models.DatasetError)
+                  .where(models.DatasetError.error_type != 'schema error')
+                  .group_by(models.Publisher)
+                  .order_by(fn.Count(models.DatasetError.id).desc()))
+    return render_template('errors.html', publishers=publishers)
