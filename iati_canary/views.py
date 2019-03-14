4064a2f8913e7f2aa42415f4a8943b36b1f55100
Andy Lulham <a.lulham@gmail.com>
2019-03-14 14:57:35 +0000

    Rejig

--- iati_canary/views.py
+++ iati_canary/views.py
@@ -10,9 +10,21 @@ blueprint = Blueprint('iati_canary', __name__,
 
 @blueprint.route('/')
 def home():
+    publishers = (models.Publisher
+                  .select(models.Publisher,
+                          fn.Count(models.DatasetError.id).alias('count'))
+                  .join(models.DatasetError)
+                  .where(models.DatasetError.error_type != 'schema error')
+                  .group_by(models.Publisher)
+                  .order_by(fn.Count(models.DatasetError.id).desc()))
+    return render_template('home.html', publishers=publishers)
+
+
+@blueprint.route('/publishers')
+def publishers():
     publishers = models.Publisher.select().order_by(
         models.Publisher.last_checked.desc(nulls='LAST'))
-    return render_template('home.html', publishers=publishers)
+    return render_template('publishers.html', publishers=publishers)
 
 
 @blueprint.route('/publisher/<publisher_id>')
@@ -22,15 +34,3 @@ def publisher(publisher_id):
     except DoesNotExist:
         return abort(404)
     return render_template('publisher.html', publisher=publisher)
-
-
-@blueprint.route('/errors')
-def errors():
-    publishers = (models.Publisher
-                  .select(models.Publisher,
-                          fn.Count(models.DatasetError.id).alias('count'))
-                  .join(models.DatasetError)
-                  .where(models.DatasetError.error_type != 'schema error')
-                  .group_by(models.Publisher)
-                  .order_by(fn.Count(models.DatasetError.id).desc()))
-    return render_template('errors.html', publishers=publishers)
