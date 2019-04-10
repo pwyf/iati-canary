031c8b38f9f955c86650c7a9aea636063e7f0312
Andy Lulham <a.lulham@gmail.com>
2019-04-10 08:28:29 +0100

    Heroku insists on this env variable naming

--- iati_canary/settings.py
+++ iati_canary/settings.py
@@ -11,7 +11,7 @@ env.read_env()
 ENV = env.str('FLASK_ENV', default='production')
 DEBUG = ENV == 'development'
 
-SQLALCHEMY_DATABASE_URI = env.str('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_DATABASE_URI = env.str('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SECRET_KEY = env.str('SECRET_KEY')
