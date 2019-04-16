7e9ad8762783075ad1f177c934509ef41ea7738b
Andy Lulham <a.lulham@gmail.com>
2019-04-16 05:52:00 +0100

    Check email is unique

--- canary/forms.py
+++ canary/forms.py
@@ -15,11 +15,22 @@ class DynamicSelectField(SelectField):
             raise ValueError(self.gettext('Please choose a publisher.'))
 
 
+class UniqueEmailField(StringField):
+    def pre_validate(self, form):
+        pub = models.Contact.where(
+            email=self.data,
+            publisher_id=form.data['publisher_id'],
+        ).first()
+        if pub:
+            msg = 'That email address is signed up for that publisher.'
+            raise ValueError(self.gettext(msg))
+
+
 class SignupForm(FlaskForm):
     render_kw = {'class': 'form-control form-control-lg'}
     name = StringField('Name', render_kw=render_kw, validators=[
         DataRequired('Please enter your name.')])
-    email = StringField('Email Address', render_kw=render_kw, validators=[
+    email = UniqueEmailField('Email Address', render_kw=render_kw, validators=[
         DataRequired('Please enter your email address.'),
         Email('Please enter a valid email address.')])
     publisher_id = DynamicSelectField('Publisher', render_kw=render_kw,
